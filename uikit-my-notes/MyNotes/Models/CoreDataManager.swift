643a03f4f600aa00c6d5aa974a2bb1ae3fae0ea4
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2023-04-14 16:21:20 +0200

    Added Saving, retrieving and deleting functionality

--- uikit-my-notes/MyNotes/Models/CoreDataManager.swift
+++ uikit-my-notes/MyNotes/Models/CoreDataManager.swift
@@ -27,4 +27,40 @@ class CoreDataManager {
             completion?()
         }
     }
+    
+    func save() {
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                print("An error ocurred while saving \(error.localizedDescription) ")
+            }
+        }
+    }
+}
+
+// MARK:-  Helper functions
+
+extension CoreDataManager {
+    func createNotre() -> Note {
+        let note = Note(context: viewContext)
+        note.id = UUID()
+        note.text = ""
+        note.lastUpdated = Date()
+        save()
+        return note
+    }
+    
+    func fetchNotes() -> [Note] {
+        let request: NSFetchRequest<Note> = Note.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(keyPath: \Note.lastUpdated, ascending: false)
+        request.sortDescriptors = [sortDescriptor]
+        return (try? viewContext.fetch(request)) ?? []
+        
+    }
+    
+    func deleteNote(_ note: Note) {
+        viewContext.delete(note)
+        save()
+    }
 }
--- uikit-my-notes/MyNotes/Views/ListNotes/ListNotesViewController.swift
+++ uikit-my-notes/MyNotes/Views/ListNotes/ListNotesViewController.swift
@@ -25,6 +25,7 @@ class ListNotesViewController: UIViewController {
         self.navigationController?.navigationBar.shadowImage = UIImage()
         tableView.contentInset = .init(top: 0, left: 0, bottom: 30, right: 0)
         configureSearchBar()
+        fetchNotesFromStorage()
     }
     
     private func indexForNote(id: UUID, in list: [Note]) -> IndexPath {
@@ -52,9 +53,8 @@ class ListNotesViewController: UIViewController {
     
     // MARK:- Methods to implement
     private func createNote() -> Note {
-        let note = Note()
         
-        // TODO Save note in database
+        let note = CoreDataManager.shared.createNotre()
         
         // Update table
         allNotes.insert(note, at: 0)
@@ -64,16 +64,14 @@ class ListNotesViewController: UIViewController {
     }
     
     private func fetchNotesFromStorage() {
-        // TODO Get all saved notes
-        print("Fetching all notes")
+        allNotes =  CoreDataManager.shared.fetchNotes()
     }
     
     private func deleteNoteFromStorage(_ note: Note) {
         // TODO delete the note
-        print("Deleting note")
-        
-        // Update the list
         deleteNote(with: note.id)
+        CoreDataManager.shared.deleteNote(note)
+        
     }
     
     private func searchNotesFromStorage(_ text: String) {
